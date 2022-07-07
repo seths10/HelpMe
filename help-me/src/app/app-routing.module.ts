e7ebe0b07f77c3c155c745aa4c57ec0463dee18b
Ghadi <ghadi.agha@turntabl.io>
2022-07-07 15:57:12 +0000

    Adding tutor pages

--- help-me/src/app/app-routing.module.ts
+++ help-me/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { TutorsComponent } from './pages/tutors/tutors.component';
 
 const routes: Routes = [
   { path: '', component: AboutUsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'tutors', component: TutorsComponent },
 ];
 
 @NgModule({
