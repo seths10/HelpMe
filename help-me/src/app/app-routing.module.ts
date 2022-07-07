e7ee8d070a9144a8adc4fe34edfe426f26082b97
Ghadi <ghadi.agha@turntabl.io>
2022-07-07 14:29:35 +0000

    about us page made

--- help-me/src/app/app-routing.module.ts
+++ help-me/src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 const routes: Routes = [
-  // { path: '', component: HomeComponent },
+  { path: '', component: AboutUsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
